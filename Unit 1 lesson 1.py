1ed36f6c7ed81fa6168782a94d0dac857d209209
Ray880533 <880533@pdsb.net>
2025-02-04 12:09:54 -0500

    Create Unit 1 lesson 1.py

---
+++ Unit 1 lesson 1.py
@@ -0,0 +1,10 @@
+# this is a code comment
+name = "Leslie"
+print("Hello", name)
+#i predict hello leslie will be outputed without a comma because there is no comma beside the hello 
+
+#modify
+# this is a code comment
+name = "Ray"
+print("Hello,", name)
+# i predict hello, Ray will be outputed with a comma because there is a comma beside hello 
