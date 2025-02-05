3b1646ec78f863cfc1ffaeea22b98590d4e02191
Ray880533 <880533@pdsb.net>
2025-02-05 14:02:16 -0500

    Update unit 1 lesson 2.py

--- unit 1 lesson 2.py
+++ unit 1 lesson 2.py
@@ -7,3 +7,23 @@ print (amount)
 
 amount = 24
 print("2 dozen is:" , amount ) 
+
+amount = 8
+amount = amount * 2
+print(amount)
+
+q = 16
+q = q - 1 # using subtraction
+q -= 1 # does the same thing
+print("The value is now: ", q) # prints out "The value is now: 16"
+q = q / 4 # using division
+q /= 2 # same thing
+print("The value is now: ", q) # prints out "The value is now: 4.0" 
+
+q = 16
+q = q - 2 # using subtraction
+q -= 2 # does the same thing
+print("The value is now: ", q) # prints out "The value is now: 18"
+q = q / 4 # using division
+q /= 4 # same thing
+print("The value is now: ", q) # prints out "The value is now: 10"
