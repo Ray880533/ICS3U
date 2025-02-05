206ce8a4886ade4d2d5bf5092532f5e372ec916d
Ray880533 <880533@pdsb.net>
2025-02-05 14:12:52 -0500

    Create unit1 lesson 3.py

---
+++ unit1 lesson 3.py
@@ -0,0 +1,10 @@
+print("Welcome to Sanjay's Koala Party!")
+print("It's possible Sanjay can distribute ", 12)
+print(" cookies for each bowl.") 
+#this will print welcome to sanjay's koala party, its possible snajay can distribute 12 cookies for each bowl
+
+
+print("Welcome to Sanjay's Koala Party!")
+print(" cookies per bowl is")
+print(144/6)
+# this specifies that cookies per bowl will be 144/6 which is 24
